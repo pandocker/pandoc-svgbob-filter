1c27a78482e905afc19cac2b25e2e2565b7ba8fb
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2018-12-09 12:48:27 +0900

    update __init__.py
    - inline filter does not require Para

--- pandoc_svgbob_filter/__init__.py
+++ pandoc_svgbob_filter/__init__.py
@@ -29,57 +29,54 @@ class SvgbobInline(object):
         assert which("rsvg-convert"), "rsvg-convert is not in path"
 
     def action(self, elem, doc):
-        if isinstance(elem, (pf.Para)) and len(elem.content) == 1:
-            for subelem in elem.content:
-                if isinstance(subelem, pf.Link) and "svgbob" in subelem.classes:
-                    fn = subelem.url
-                    options = subelem.attributes
-                    caption = subelem.content
+        if isinstance(elem, pf.Link) and "svgbob" in elem.classes:
+            fn = elem.url
+            options = elem.attributes
+            caption = elem.content
 
-                    meta_font_family = doc.get_metadata("svgbob.font-family", "Arial")
-                    meta_font_size = doc.get_metadata("svgbob.font-size", 14)
-                    meta_scale = doc.get_metadata("svgbob.scale", 1)
-                    meta_stroke_width = doc.get_metadata("svgbob.stroke-width", 2)
+            meta_font_family = doc.get_metadata("svgbob.font-family", "Arial")
+            meta_font_size = doc.get_metadata("svgbob.font-size", 14)
+            meta_scale = doc.get_metadata("svgbob.scale", 1)
+            meta_stroke_width = doc.get_metadata("svgbob.stroke-width", 2)
 
-                    font_family = options.get("font-family", meta_font_family)
-                    font_size = options.get("font-size", meta_font_size)
-                    scale = options.get("scale", meta_scale)
-                    stroke_width = options.get("stroke-width", meta_stroke_width)
-                    svgbob_option = " ".join([
-                        '--font-family "{}"'.format(font_family) if font_family is not None else "",
-                        "--font-size {}".format(font_size) if font_size is not None else "",
-                        "--scale {}".format(scale) if scale is not None else "",
-                        "--stroke-width {}".format(stroke_width) if stroke_width is not None else "",
-                    ])
-                    if not os.path.exists(self.dir_to):
-                        os.mkdir(self.dir_to)
+            font_family = options.get("font-family", meta_font_family)
+            font_size = options.get("font-size", meta_font_size)
+            scale = options.get("scale", meta_scale)
+            stroke_width = options.get("stroke-width", meta_stroke_width)
+            svgbob_option = " ".join([
+                '--font-family "{}"'.format(font_family) if font_family is not None else "",
+                "--font-size {}".format(font_size) if font_size is not None else "",
+                "--scale {}".format(scale) if scale is not None else "",
+                "--stroke-width {}".format(stroke_width) if stroke_width is not None else "",
+            ])
+            if not os.path.exists(self.dir_to):
+                os.mkdir(self.dir_to)
 
-                    data = open(fn, "r", encoding="utf-8").read()
-                    counter = hashlib.sha1(data.encode("utf-8")).hexdigest()[:8]
-                    self.basename = "/".join([self.dir_to, str(counter)])
+            data = open(fn, "r", encoding="utf-8").read()
+            counter = hashlib.sha1(data.encode("utf-8")).hexdigest()[:8]
+            self.basename = "/".join([self.dir_to, str(counter)])
 
-                    if doc.format in ["latex"]:
-                        format = "pdf"
-                    elif doc.format in ["html", "html5"]:
-                        format = "svg"
-                    else:
-                        format = "png"
+            if doc.format in ["latex"]:
+                format = "pdf"
+            elif doc.format in ["html", "html5"]:
+                format = "svg"
+            else:
+                format = "png"
 
-                    linkto = os.path.abspath(".".join([self.basename, format])).replace("\\", "/")
+            linkto = os.path.abspath(".".join([self.basename, format])).replace("\\", "/")
 
-                    command = "svgbob {} {} | rsvg-convert -f {} -o {}".format(fn, svgbob_option, format, linkto)
-                    # pf.debug(command)
-                    sp.Popen(command, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+            command = "svgbob {} {} | rsvg-convert -f {} -o {}".format(fn, svgbob_option, format, linkto)
+            # pf.debug(command)
+            sp.Popen(command, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
 
-                    pf.debug("[inline] generate svgbob from {}".format(fn))
-                    subelem.classes.remove("svgbob")
-                    img = pf.Image(*caption, classes=subelem.classes, url=linkto,
-                                   identifier=subelem.identifier, title="fig:", attributes=subelem.attributes)
+            pf.debug("[inline] generate svgbob from {}".format(fn))
+            elem.classes.remove("svgbob")
+            elem = pf.Image(*caption, classes=elem.classes, url=linkto,
+                            identifier=elem.identifier, title="fig:", attributes=elem.attributes)
 
-                    ans = pf.Para(img)
-                    # pf.debug(ans)
+            # pf.debug(elem)
 
-                    return ans
+            return elem
 
 
 def main(doc=None):
