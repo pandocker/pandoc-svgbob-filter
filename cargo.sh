9c40b22e5bcf3b3e9b533deeb9b5b0805a74f7dd
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2020-08-23 01:46:54 +0900

    update build script and Makefile target

--- cargo.sh
+++ cargo.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env sh
 
+apt update
+apt -y install gcc-mingw-w64
+rustup update
+
 rustup target add x86_64-unknown-linux-musl
 cargo install --force svgbob_cli --root=svgbob --target=x86_64-unknown-linux-musl
+
+rustup target add x86_64-pc-windows-gnu
+cargo install --force svgbob_cli --root=svgbob_win --target=x86_64-pc-windows-gnu
+
+rustup target add x86_64-apple-darwin
+cargo install --force svgbob_cli --root=svgbob_osx --target=x86_64-apple-darwin
